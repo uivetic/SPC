6cde4796602f626b334d265bcc73ec5f4be360ae
urosivetic <uivetic@gmail.com>
2025-03-27 19:02:43 +0100

    started work on opste combo box

--- gui.py
+++ gui.py
@@ -7,6 +7,7 @@ from google.oauth2.service_account import Credentials
 import unicodedata
 import sys
 import os
+from PyQt5.QtWidgets import QComboBox
 
 
 # resava qt.qpa.wayland: Wayland does not support QWindow::requestActivate()
@@ -47,57 +48,65 @@ class MyWindow(QMainWindow, Ui_MainWindow):
         completer.setCaseSensitivity(False)
 
 
+        # completer za imena
+        self.nameLineEditLeft.setCompleter(completer)
 
-        self.lineEdit.setCompleter(completer)
-        self.pushButton.clicked.connect(self.go_to_spc)
-        self.nazadButton.clicked.connect(self.go_to_mainMenu)
-        self.comboBoxProjekat.currentIndexChanged.connect(self.on_project_changed)
-        self.comboBoxUloga.currentIndexChanged.connect(self.on_role_changed)
-        self.comboBoxUloga.setEnabled(False)
-        self.comboBoxBodovi.setEnabled(False)
+        # dugmad za ulazak i izlazak iz upisivanja
+        self.spcPushButton.clicked.connect(self.go_to_spc)
+        # self.backButtonLeft.clicked.connect(self.go_to_mainMenu)
+        
+        # opste combo-boxevi
 
-    def go_to_spc(self):
-        self.stackedWidget.setCurrentIndex(1)
+        print(type(self.dropDownOpste1))
+        print(type(self.dropDownOpste2))
+        print(type(self.dropDownOpste3))
+        print(type(self.dropDownOpste4))
+
+        self.dropDownOpste1.addItems(["", "Option 1", "Option 2", "Option 3"])
+        self.dropDownOpste2.addItems(["", "A", "B", "C"])
+        self.dropDownOpste3.addItems(["", "X", "Y", "Z"])
+        self.dropDownOpste4.addItems(["", "Red", "Blue", "Green"])
+
+        self.dropDownOpste2.setEnabled(False)
+        self.dropDownOpste3.setEnabled(False)
+        self.dropDownOpste4.setEnabled(False)
+
+        # Connect signals to update next combo box
+        self.dropDownOpste1.currentIndexChanged.connect(self.update_dropDownOpste2)
+        self.dropDownOpste2.currentIndexChanged.connect(self.update_dropDownOpste3)
+        self.dropDownOpste3.currentIndexChanged.connect(self.update_dropDownOpste4)
+
+
+    def update_dropDownOpste2(self):
+        """Enable dropDownOpste2 if dropDownOpste1 has a selection"""
+        self.dropDownOpste2.setEnabled(bool(self.dropDownOpste1.currentText()))
+        if not self.dropDownOpste1.currentText():  # Reset subsequent selections
+            self.dropDownOpste2.setCurrentIndex(0)
+            self.dropDownOpste3.setCurrentIndex(0)
+            self.dropDownOpste4.setCurrentIndex(0)
+            self.dropDownOpste3.setEnabled(False)
+            self.dropDownOpste4.setEnabled(False)
+
+    def update_dropDownOpste3(self):
+        """Enable dropDownOpste3 if dropDownOpste2 has a selection"""
+        self.dropDownOpste3.setEnabled(bool(self.dropDownOpste2.currentText()))
+        if not self.dropDownOpste2.currentText():
+            self.dropDownOpste3.setCurrentIndex(0)
+            self.dropDownOpste4.setCurrentIndex(0)
+            self.dropDownOpste4.setEnabled(False)
+
+    def update_dropDownOpste4(self):
+        """Enable dropDownOpste4 if dropDownOpste3 has a selection"""
+        self.dropDownOpste4.setEnabled(bool(self.dropDownOpste3.currentText()))
+        if not self.dropDownOpste3.currentText():
+            self.dropDownOpste4.setCurrentIndex(0)
+
     
-    def go_to_mainMenu(self):
-        self.stackedWidget.setCurrentIndex(0)
-    
-    def on_project_changed(self, index):
-        print("Selected:", self.comboBoxProjekat.itemText(index))
-        if len(self.comboBoxProjekat.itemText(index)) > 0:
-            self.comboBoxUloga.setEnabled(True)
-        else:
-            #self.comboBoxUloga.setText("")
-            self.comboBoxUloga.setEnabled(False)
-            self.comboBoxUloga.setCurrentIndex(0)
-
-    
-    def on_role_changed(self, index):
-        #items = self.get_combo_items(self.comboBoxUloga)
-        if self.comboBoxUloga.itemText(index) == "PR Tim" or self.comboBoxUloga.itemText(index) == "FR Tim":
-            self.comboBoxBodovi.clear()
-            self.comboBoxBodovi.setEnabled(True)
-            self.comboBoxBodovi.insertItem(0)
-            self.comboBoxBodovi.insertItem(1)
-            self.comboBoxBodovi.insertItem(2)
-
-        if self.comboBoxUloga.itemText(index) == "CT":
-            self.comboBoxBodovi.clear()
-            self.comboBoxBodovi.setEnabled(True)
-            if self.comboBoxProjekat.itemText(index) == "JobFair" or self.comboBoxProjekat.itemText(index) == "Kurs":
-                self.comboBoxBodovi.insertItem("4")
-                self.comboBoxBodovi.insertItem("8")
-                self.comboBoxBodovi.insertItem("12")
-                self.comboBoxBodovi.insertItem("16")
-                self.comboBoxBodovi.insertItem("20")
-                self.comboBoxBodovi.insertItem("24")
-            else:
-                self.comboBoxBodovi.insertItem("4")
-                self.comboBoxBodovi.insertItem("8")
-                self.comboBoxBodovi.insertItem("12")
 
     def get_combo_items(comboBox):
         return [comboBox.itemText(i) for i in range(comboBox.count())]
+    def go_to_spc(self):
+        self.stackedWidget.setCurrentIndex(1)
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MyWindow()
